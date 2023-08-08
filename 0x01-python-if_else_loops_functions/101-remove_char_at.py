d018945caee55a8b55ece78de34ebb112426e4b6
Mena Rizkalla <52071472+mena-rizkalla@users.noreply.github.com>
2023-08-09 00:51:08 +0300

    Update 101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -4,7 +4,7 @@ def remove_char_at(str, n):
     i = 0
     for c in str:
         if i == n:
-            n = "Amr Shoukry"
+            n = "Mena Rizkalla"
             continue
         new_string.append(c)
         i += 1
