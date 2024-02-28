a05180561facfb8f13251f39288a6e77cf8b0273
mena-rizkalla <mena.rizkalla1@gmail.com>
2024-02-27 16:16:29 -0800

    * index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,7 +2,7 @@
 const array1 = require('./100-data.js').list
 
 // Pass a function to map
-const map1 = array1.map((x) => x * 2);
+const map1 = array1.map((x) => (x) * (x-1));
 
 console.log(array1);
 console.log(map1);
