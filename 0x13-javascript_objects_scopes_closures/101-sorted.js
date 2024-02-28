d99cf072987d5dd238c1ca2d5b46798a9fc319ce
mena-rizkalla <mena.rizkalla1@gmail.com>
2024-02-27 16:37:51 -0800

    sorted file

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,4 +1,11 @@
 #!/usr/bin/node
-const dictionary = require('./101-data.js').dict;
-const dic = {};
-
+const dict = require('./101-data.js').dict;
+const myDict = {
+	1:[],
+	2:[],
+	3:[]
+};
+for (const key in dict) {
+    const value = myDict[key];
+    myDict[value].push(key);
+}
