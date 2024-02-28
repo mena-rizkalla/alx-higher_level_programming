eb79ede0d4b31eee91f4981ad35d3481581eb5b3
mena-rizkalla <mena.rizkalla1@gmail.com>
2024-02-27 16:29:33 -0800

    index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const array1 = require('./100-data.js').list
+const array1 = require('./100-data.js').list;
 const map1 = array1.map((x, index) => x * index);
 console.log(array1);
 console.log(map1);
---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+const dictionary = require('./101-data.js').dict;
+const dic = {};
+
